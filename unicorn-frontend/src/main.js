99704e1b960faf057a2981967cf6060269022be4
rkamruzzamanripon <rkamruzzaman@gmail.com>
2024-11-29 03:36:03 +0600

    add url

--- unicorn-frontend/src/main.js
+++ unicorn-frontend/src/main.js
@@ -10,7 +10,8 @@ import router from './router'
 const app = createApp(App)
 
 // Set Axios base URL and default headers
-axios.defaults.baseURL = 'http://127.0.0.1:8888';
+// axios.defaults.baseURL = 'http://127.0.0.1:8888';
+axios.defaults.baseURL = 'http://160.191.163.33:8888';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 
