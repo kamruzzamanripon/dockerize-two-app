6d2810fe0d855409d8bf9cbbf9355041331b7d29
rkamruzzamanripon <rkamruzzaman@gmail.com>
2024-12-09 16:41:37 +0600

    kubernet bug fix

--- unicorn-frontend/src/main.js
+++ unicorn-frontend/src/main.js
@@ -12,8 +12,10 @@ const app = createApp(App)
 // Set Axios base URL and default headers
 // axios.defaults.baseURL = 'http://127.0.0.1:8888';
 //axios.defaults.baseURL = 'http://160.191.163.33:8888';
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:8888';
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+console.log('env url-1', backendUrl);
 axios.defaults.baseURL = backendUrl;
+console.log('env url-1', axios.defaults.baseURL);
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 
